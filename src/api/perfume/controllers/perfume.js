99b2c70128f9c145ec3a1729d285c53eff444d49
Aleksa Hadžić <122030667+walkingkindle@users.noreply.github.com>
2023-08-27 15:16:59 +0200

    Add files via upload

---
+++ src/api/perfume/controllers/perfume.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * perfume controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::perfume.perfume');
---
+++ src/api/perfume/routes/perfume.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * perfume router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::perfume.perfume');
